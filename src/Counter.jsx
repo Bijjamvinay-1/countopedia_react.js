00656f3de8e65ebb721eaee85d3e5c997d28bdff
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2024-06-02 11:17:32 +0530

    Onclick Button handler

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 export default class Counter extends React.Component {
-  render() {
-    return <div className="text-white">Counter</div>;
+
+  handleAttack(){
+    alert("Attack clicked")
   }
-}
+
+  defendAttack(){
+    alert("Defend clicked")
+  }
+  render() {
+    return(
+        <div className="row text-white">
+          <h1>Counter:</h1>
+          <button onClick={this.handleAttack} style={{width:"200px"}}>
+            +1
+            </button>
+          <button onClick={this.defendAttack}style={{width:"200px"}}>
+            -1
+            </button>
+        </div>
+    );
+} }
